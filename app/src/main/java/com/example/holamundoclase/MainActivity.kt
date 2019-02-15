af442ea10ecfcccac7d8c497a180471485e2a7b9
Mdaniel23 <47264726+Mdaniel23@users.noreply.github.com>
2019-02-14 20:14:02 -0800

    Initial commit

---
+++ app/src/main/java/com/example/holamundoclase/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.holamundoclase
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+
+
+}
